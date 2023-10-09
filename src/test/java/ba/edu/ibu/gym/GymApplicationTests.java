5de38cad1041809df3079309833aafba3a9575d6
Kerim <kerim.sabic.207@gmail.com>
2023-10-09 18:30:24 +0200

    initial commit

---
+++ src/test/java/ba/edu/ibu/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
